62682d7d7e97a56e6c945c96cb0532dc077e672d
nato1212 <natananamsalu@gmail.com>
2025-02-20 09:48:53 +0300

    alx-react-app-new added

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        backgroundColor: "gray",
+        color: "black",
+        textAlign: "center",
+        fontSize: "20px",
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
