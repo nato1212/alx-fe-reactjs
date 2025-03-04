aff537b689cbc1d111be8650f48c039b6b2af8c9
nato1212 <natananamsalu@gmail.com>
2025-03-04 16:38:48 +0300

    searchBar recipe

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
@@ -13,6 +14,8 @@ const App = () => {
             path="/"
             element={
               <>
+                <SearchBar />
+
                 <AddRecipeForm />
                 <RecipeList />
               </>
--- recipe-sharing-app/src/store/recipeStore.js
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -1,19 +1,19 @@
-import { create } from "zustand";
+import create from "zustand";
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: "",
+  setSearchTerm: (term) => set({ searchTerm: term }),
   addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  deleteRecipe: (id) =>
     set((state) => ({
-      recipes: [...state.recipes, newRecipe],
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
-    })),
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
     })),
 }));
