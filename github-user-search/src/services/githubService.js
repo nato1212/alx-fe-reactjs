8ba07314dd781296c5295908d0cb493981776092
nato1212 <natananamsalu@gmail.com>
2025-03-25 10:32:27 +0300

    no update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,26 @@
 import axios from "axios";
 
-const GITHUB_API_URL = "https://api.github.com/search/users?q=";
+const GITHUB_API_URL = "https://api.github.com/users/";
+const SEARCH_API_URL = "https://api.github.com/search/users?q=";
 const API_KEY = import.meta.env.VITE_GITHUB_API_KEY;
+
+export const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}${username}`, {
+      headers: {
+        Authorization: `token ${API_KEY}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching GitHub user data:", error);
+    return null;
+  }
+};
 
 export const fetchUsers = async (query) => {
   try {
-    const response = await axios.get(`${GITHUB_API_URL}${query}`, {
+    const response = await axios.get(`${SEARCH_API_URL}${query}`, {
       headers: {
         Authorization: `token ${API_KEY}`,
       },
